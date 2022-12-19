8561395d7af813dfc38fb203d0cd12fbb684ef77
Robert Buskirk-Lechner <buskirklechner@gmail.com>
2022-12-19 10:47:12 -0500

    axios install and intitial set-up

--- venue_client/src/App.js
+++ venue_client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import VenueList from './components/VenueList';
 
 function App() {
   return (
     <div className="App">
       <h1>Venue_Stackathon</h1>
+      <VenueList />
     </div>
   );
 }

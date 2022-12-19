95576e9df0458c1801e1b269ace87b893f9dd6d1
Ryan Saucier <ryanyoubum@gmail.com>
2022-12-19 11:29:39 -0500

    event list added to front end

--- venue_client/src/App.js
+++ venue_client/src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
-import VenueList from './components/VenueList';
+import "./App.css";
+import VenueList from "./components/VenueList";
+import EventList from "./components/EventList";
 
 function App() {
   return (
     <div className="App">
       <h1>Venue_Stackathon</h1>
       <VenueList />
+      <EventList />
     </div>
   );
 }

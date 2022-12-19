714f6fa2b5362e1296b536e8eca2e9055ccb781e
Robert Buskirk-Lechner <buskirklechner@gmail.com>
2022-12-19 11:45:36 -0500

    Adding Main component to client

---
+++ venue_client/src/components/Main.jsx
@@ -0,0 +1,24 @@
+import axios from 'axios'
+import { useEffect } from 'react'
+import { useState, uesEffect } from 'react'
+export default function Main () {
+    const [venues, setVenues] = useState([])
+    const [events, setEvents] = useState([])
+    const getVenues = async () => {
+        const response = await axios.get('localhost:8000/venues/')
+        setVenues(response.data)
+        console.log(venues)
+    }
+    const getEvents = async () => {
+        const response = await axios.get('localhost:8000/events/')
+        setVenues(response.data)
+        console.log(venues)
+    }
+    useEffect(()=> {
+    getVenues()
+    },[])
+    return (
+        <div>
+        </div>
+    )
+}
